f5a8aa173d816611f44f0ea65d700435fdf7504e
Sabreena Salomon <salomonbree@gmail.com>
2024-07-03 14:21:51 +0000

    defined personality_traits, edited printed strings

--- horoscope.rb
+++ horoscope.rb
@@ -20,23 +20,24 @@ class Horoscope
   def zodiac_signs
     loop do
       puts "\n"
-      puts "To begin I will need to know your zodiac. What is your zodiac sign #{self.name}?"
+      puts "To begin I will need to know your zodiac. What is your zodiac sign?"
       puts "\n"
-      puts "1. Capricorn(December 22 - January 19)"
-      puts "2. Aquarius(January 20 - February 18)"
-      puts "3. Pisces(February 19 - March 20)"
-      puts "4. Aries(March 21 - April 19)"
-      puts "5. Taurus(April 20 - May 20)"
-      puts "6. Gemini(May 21 - June 20)"
-      puts "7. Cancer(June 21 - July 22)"
-      puts "8. Leo(July 23 - August 22)"
-      puts "9. Virgo(August 23 - September 22)"
-      puts "10. Libra(September 23 - October 22)"
-      puts "11. Scorpio(October 23 - November 21)"
-      puts "12. Sagittarius(November 21 - December 21)"
-      puts "Choose a zodiac sign: "
-      self.sign = gets.chomp.to_i
-    
+      puts " ~Capricorn(December 22 - January 19)"
+      puts " ~Aquarius(January 20 - February 18)"
+      puts " ~Pisces(February 19 - March 20)"
+      puts " ~Aries(March 21 - April 19)"
+      puts " ~Taurus(April 20 - May 20)"
+      puts " ~Gemini(May 21 - June 20)"
+      puts " ~Cancer(June 21 - July 22)"
+      puts " ~Leo(July 23 - August 22)"
+      puts " ~Virgo(August 23 - September 22)"
+      puts " ~Libra(September 23 - October 22)"
+      puts " ~Scorpio(October 23 - November 21)"
+      puts " ~Sagittarius(November 21 - December 21)"
+      puts "Type your zodiac sign below: "
+      self.sign = gets.chomp.capitalize
+      puts "\n"
+      puts "Okay #{self.name} you are a #{self.sign}!"
       choice_commands
     end
   end
@@ -61,7 +62,7 @@ def choice_commands
       puts "Thank you for checking your horoscope #{self.name}! See you next time!"
       exit
     else
-      puts "That is not a zodiac sign. Please choose another option."
+      puts "That is not a option. Please choose another option."
       choice_commands
     end
   end
@@ -69,27 +70,16 @@ end
 
 def monthly_horoscope
   puts "\n"
-  quotes = ["**You will have a productive month!**", "**You will have bad luck this month. Try to stay positive!**", "**You will try many new things this month!**", "**This month you will see someone from your past.**", "**This month a great opprotunity awaits! Take it!**", "**Avoid distractions this month. They will be abundant. Stay focused!**", "**A new family member will arrive in your life this month!**", "**You will come across an old hobby this month! Dive back in!**"]
+  quotes = ["**You will have a productive month!**", "**You will have bad luck this month. Try to stay positive!**", "**You will try many new things this month!**", "**This month you will see someone from your past.**", "**This month a great opprotunity awaits! Take it!**", "**Avoid distractions this month. They will be abundant. Stay focused!**", "**A new family member will arrive in your life this month!**", "**You will come across an old hobby this month! Dive back in!**", "**Something this month will drastically change your bank account.**"]
   puts quotes.sample
   choices
 end
 
 def personality_traits
-  if self.sign = 1
-      capricorn
-  elsif self.sign = 2
-       aquarius
-    end 
-end
-
-def capricorn
+  traits = ["competetive but insecure", "loyal but stubborn", "versatile but impatient", "passionate but uncommunicative", "confident and dominating", "perfectionist but self-critical", "empathetic but indecisive", "intense but secretive", "spontaneous but flighty", "goal-oriented but unforgiving", "philisophical but detached", "whimsical but over-sensitive"]
   puts "\n"
-  puts "Capricorns are very fun!"
-end
-
-def aquarius
-  puts "\n"
-  puts "Aquarius signs love sports!"
+  puts "When you are a #{self.sign} you tend to be #{traits.sample}."
+  choices
 end
 
 def choices
